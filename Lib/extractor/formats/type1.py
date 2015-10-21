f30895ab80009da4674d4f62fc3f7c75b0b56871
Cosimo Lupo <cosimo.lupo@daltonmaag.com>
2015-10-21 10:03:02 +0100

    [type1] remove AppKit dependency; fontTools.t1Lib can detect type

--- Lib/extractor/formats/type1.py
+++ Lib/extractor/formats/type1.py
@@ -10,17 +10,18 @@ from extractor.tools import RelaxedInfo
 # ----------------
 # Public Functions
 # ----------------
-    
+
 def isType1(pathOrFile):
     try:
-        font = _readT1Font(pathOrFile)
+        font = T1Font(pathOrFile)
         del font
     except T1Error:
         return False
     return True
 
+
 def extractFontFromType1(pathOrFile, destination, doGlyphs=True, doInfo=True, doKerning=True, doGroups=True, doFeatures=True, doLib=True, customFunctions=[]):
-    source = _readT1Font(pathOrFile)
+    source = T1Font(pathOrFile)
     destination.lib["public.glyphOrder"] = _extractType1GlyphOrder(source)
     if doInfo:
         extractType1Info(source, destination)
@@ -39,29 +40,6 @@ def extractType1Info(source, destination):
     _extractType1FontInfo(source, info)
     _extractType1Private(source, info)
     _extractType1FontMatrix(source, info)
-
-# ----
-# Read
-# ----
-
-def _readT1Font(pathOrFile):
-    ## rewrite the fontTools read() cause it was using lots of carbon stuff not avialble in py 64bit
-    import AppKit
-    fileType, error = AppKit.NSWorkspace.sharedWorkspace().typeOfFile_error_(pathOrFile, None)
-    normpath = pathOrFile.lower()
-    
-    readFunc = None
-    if fileType == "com.adobe.postscript-lwfn-font":
-        readFunc = readLWFN
-    elif fileType == "com.apple.font-suitcase":
-        readFunc = readLWFN
-    elif normpath[-4:] == '.pfb':
-        readFunc = readPFB
-    else:
-        readFunc = readOther
-    font = T1Font()
-    font.data = readFunc(pathOrFile)
-    return font
 
 # ----
 # Info
